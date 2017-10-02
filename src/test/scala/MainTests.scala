d5a277c487ecbf36815909d199af917f226f8442
gnanesh <gnaneshkunal@outlook.com>
2017-10-02 21:22:03 +0530

    added tests

--- src/test/scala/MainTests.scala
+++ src/test/scala/MainTests.scala
@@ -1,3 +1,32 @@
-object MainTests {
+package MainTests
 
+import utest._
+import scalanative.native._
+
+object MainTests extends TestSuite {
+
+  val c = libc.malloc(20)
+
+  hello.greet(c, 20)
+
+  stdio.puts(c)
+
+  val tests = Tests {
+    'HelloWorldTest - {
+      c == c"Hello, World"
+    }
+  }
+  libc.free(c)
+}
+
+@extern
+@link("hello")
+object hello {
+  def greet(str: CString, n: CInt): CString = extern
+}
+
+@extern
+object libc {
+  def malloc(size: CSize): Ptr[Byte] = extern
+  def free(ptr: Ptr[Byte]): Unit = extern
 }
